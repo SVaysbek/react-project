ac76b3e329fc3cd69fdd8ae6fa21a265ba4658a2
Mirjalol <mirjalol0406@gmail.com>
2022-11-11 18:38:32 +0500

    navbar-footer update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Pizza from './components/pages/Pizza'
 import Footer from './components/footer/Footer'
 
